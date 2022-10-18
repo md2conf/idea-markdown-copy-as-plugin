85e0c4de4eb9137b14ea36a68464b89ad4af92b1
GitHub Action <action@github.com>
2022-10-18 09:57:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/qwazer/ideamarkdowncopyasplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/qwazer/ideamarkdowncopyasplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.qwazer.ideamarkdowncopyasplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qwazer.ideamarkdowncopyasplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/qwazer/ideamarkdowncopyasplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/qwazer/ideamarkdowncopyasplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.qwazer.ideamarkdowncopyasplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qwazer.ideamarkdowncopyasplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
